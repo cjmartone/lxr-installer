1fad9a383b86893d1395e7c19873000c4a8acf94
CJ Martone <martonecj@gmail.com>
2020-01-29 14:00:57 -0500

    change lxr location

--- lxr.sh
+++ lxr.sh
@@ -3,17 +3,21 @@
 apt-get install -y apache2 mysql-server exuberant-ctags glimpse
 apt-get install -y libdbi-perl libdbd-mysql-perl libfile-mmagic-perl libapache2-mod-perl2
 
-wget https://sourceforge.net/projects/lxr/files/stable/lxr-2.3.5.tgz/download
+wget https://sourceforge.net/projects/lxr/files/stable/lxr-2.3.5.tgz
 
-tar -zxf ~/Download/lxr-2.3.5.tgz
-mv ~/Download/lxr-2.3.5 /usr/local/share/lxr
+tar -zxf lxr-2.3.5.tgz
+mv lxr-2.3.5 lxr
 
-./usr/local/share/lxr/scripts/configure-lxr.pl
-./usr/local/share/lxr/custom.d/initdb.sh
-cp /usr/local/share/lxr/custom.d/lxr.conf /usr/local/share/lxr/
+cd lxr
+./scripts/configure-lxr.pl
+./custom.d/initdb.sh
+cp custom.d/lxr.conf .
 
 wget https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-5.4.16.tar.xz
-tar xf ~/Download/linux-5.4.16.tar.xz
-mv ~/Download/linux-5.4.16 /home/myself/kernel/v1
+tar linux-5.4.16.tar.xz
+mkdir kernel
+mkdir kernel/v1
+mkdir glimpse_DB
+mv linux-5.4.16 kernel/v1
 
 ./usr/local/share/lxr/genxref --url=http://localhost/lxr
